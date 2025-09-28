564e85c1685c3f3ae966c5be3ae83dbbd3d8bd9b
srmchem <srmchem@yahoo.com>
2025-09-27 23:06:02 -0500

    Updating the required virtualbox provider

--- modules/virtualbox-vm/provider.tf
+++ modules/virtualbox-vm/provider.tf
@@ -3,9 +3,7 @@ terraform {
   required_providers {
     virtualbox = {
       source  = "terra-farm/virtualbox"
-      version = "~> 0.0.7"
+      version = "~> 0.0.3"
     }
   }
 }
-
-provider "virtualbox" {}
